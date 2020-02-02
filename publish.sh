765e825285fd289a690e7a8c426cb83b05f811ec
12buntu <hollarskyle@gmail.com>
2020-02-02 14:57:40 -0500

    I don't wanna

--- publish.sh
+++ publish.sh
@@ -2,9 +2,27 @@
 git branch --list  >> .branches
 cat .branches
 rm .branches
-echo -e "\nEnter the branch from above that you wish to use or create new branch (If you are Luke just push to master, if you are not just create a new branch with your name)\n"
+echo -e "\nType the name of an above branch to use it, or type "new" to create a new branch (If you are Luke just push to master, if you are not just create a new branch with your name)\n"
 read branch
-git checkout -b $branch
+
+#if [ $branch == "create" } ]
+#then
+#	echo -e "\n Branch Name:\n"
+#	read createBranch
+#	git checkout -b $createBranch
+#	$createBranch = $branch
+#else
+#	git checkout $branch
+#fi
+
+case "$branch" in
+    new) 
+	echo -e "\nBranch Name:\n"
+	read branchNew
+	git checkout -b $branchNew;;
+    *) git checkout $branch ;;
+esac
+
 git add --all
 git add -uf
 echo -e "\nEnter a description of you're changes or today's date:\n"
