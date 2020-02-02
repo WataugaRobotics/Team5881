1151ee59b5949cc5025317bc17cd09ded10a52c4
12buntu <hollarskyle@gmail.com>
2020-02-02 14:39:17 -0500

    script made

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 git branch --list  >> .branches
-#echo $(<.branches)
+cat .branches
+rm .branches
 echo -e "\nEnter the branch from above that you wish to use or create new branch (If you are Luke just push to master, if you are not just create a new branch with your name)\n"
+read branch
+git checkout -b $branch
 git add --all
 git add -uf
 echo -e "\nEnter a description of you're changes or today's date:\n"
